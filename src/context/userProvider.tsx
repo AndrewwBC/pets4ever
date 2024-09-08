86303dda2a49fa438f93d2df6c31fb76a0e94750
AndrewwBC <andrew-hot2011@hotmail.com>
2024-09-08 15:51:35 -0300

    fix

--- src/context/userProvider.tsx
+++ src/context/userProvider.tsx
@@ -1,7 +1,6 @@
 import { createContext, ReactNode, useContext, useMemo, useState } from "react";
 import { UserProps } from "../api/user/types/profileResponse";
 import USER_API from "../api/user/USER_API";
-import { FullDogLoader } from "../components/FullDogLoader";
 
 interface UserContextProps {
   user: UserProps | null;
@@ -17,11 +16,9 @@ const UserContext = createContext<UserContextProps>({
 
 function UserProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<UserProps | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
 
   async function retrieveUser() {
     try {
-      setIsLoading(true);
       const data = await USER_API.user();
       setUser(data);
     } catch (error) {
@@ -29,7 +26,6 @@ function UserProvider({ children }: { children: ReactNode }) {
     } finally {
       const timer = setTimeout(() => {
         console.log("Entrou no timer");
-        setIsLoading(false);
         clearTimeout(timer);
       }, 2000);
     }
@@ -55,8 +51,6 @@ function UserProvider({ children }: { children: ReactNode }) {
     }),
     [user]
   );
-
-  if (isLoading) return <FullDogLoader />;
 
   return (
     <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
