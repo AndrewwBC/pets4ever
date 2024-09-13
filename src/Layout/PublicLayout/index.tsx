889307517ca21e5b1864f26ff7c283fc66c447c8
AndrewwBC <andrew-hot2011@hotmail.com>
2024-09-13 18:22:04 -0300

    add: change theme in public routes

--- src/Layout/PublicLayout/index.tsx
+++ src/Layout/PublicLayout/index.tsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { useTheme } from "../../context/MyThemeProvider";
+import { ToggleThemeButton } from "./styles";
+import { CgMoon, CgSun } from "react-icons/cg";
 
 export function PublicRoutesLayout() {
+  const { setSystemTheme, systemTheme } = useTheme();
+
   return (
     <>
-      <main>
+      <main style={{ position: "relative" }}>
+        <ToggleThemeButton onClick={() => setSystemTheme()}>
+          {systemTheme === "light" ? (
+            <CgMoon color="#222" size={32} />
+          ) : (
+            <CgSun color="yellow" size={32} />
+          )}
+        </ToggleThemeButton>
         <Outlet />
       </main>
     </>
